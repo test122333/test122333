9fffe4df2173ff6929f3e63097278501fb2a1bec
test122333 <abc12345@gmail.com>
2014-09-04 19:36:31 +0700

    oo

---
+++ src/app/ry/profile/ImageActivity.java
@@ -0,0 +1,13 @@
+package app.ry.profile;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+public class ImageActivity extends Activity{
+ @Override
+protected void onCreate(Bundle savedInstanceState) {
+	 //TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.image_thanyanan);
+}
+}
